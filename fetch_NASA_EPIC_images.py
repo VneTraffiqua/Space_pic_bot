ff67f851dfdb6c702d9f9ec8738f2fdbfdd60f0d
VneTraffiqua <safe72again@gmail.com>
2022-07-14 08:28:53 -0700

    Create fetch_NASA_EPIC_images

---
+++ fetch_NASA_EPIC_images.py
@@ -0,0 +1,35 @@
+import requests
+import urllib.parse
+import os
+from dotenv import load_dotenv
+import main
+from pathlib import Path
+
+
+def get_EPIC_url():
+    url1 = 'https://api.nasa.gov/EPIC/api/natural/images'
+    settings = {
+        'api_key': f'{nasa_token}',
+        'start_date': f'{start_data_epic}'
+    }
+    response = requests.get(url1, params=settings)
+    response.raise_for_status()
+    return response.json()
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    nasa_token = os.getenv('NASA_TOKEN')
+    start_data_epic = os.getenv('start_date_EPIC')
+    Path('./images').mkdir(parents=True, exist_ok=True)
+    for img_num, EPIC_data in enumerate(get_EPIC_url(), 1):
+        image_id = EPIC_data['image']
+        image_date = EPIC_data['date'].split()[0].split('-')
+        EPIC_url = f'https://api.nasa.gov/EPIC/archive/natural/' \
+                   f'{image_date[0]}/{image_date[1]}/{image_date[2]}/' \
+                   f'png/{image_id}.png'
+        path = f'./images/NASA_EPIC_{img_num}.png'
+        settings = {
+            'api_key': f'{nasa_token}',
+        }
+        main.get_images(path, EPIC_url, settings)
