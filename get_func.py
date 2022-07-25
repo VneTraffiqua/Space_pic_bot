ac62bafed8c935d3f0e3eddf8eb7494de1c38140
VneTraffiqua <safe72again@gmail.com>
2022-07-25 14:04:41 -0700

    Update get_func

--- get_func.py
+++ get_func.py
@@ -8,10 +8,24 @@ def get_extension(user_url):
     return os.path.splitext(parsed_url.path)[1]
 
 
+def get_img_names(path):
+    for img_name in os.listdir(path):
+        if os.path.isfile(os.path.join(path, img_name)):
+            yield img_name
+
+
 def get_images(file_path, images_url, settings=None):
     response = requests.get(images_url, params=settings)
     response.raise_for_status()
 
     with open(file_path, 'wb') as file:
         file.write(response.content)
+
+
+def get_img_names(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
+
+
 
