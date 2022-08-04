e311986209407b735835beefe202ef64dcfa5708
VneTraffiqua <safe72again@gmail.com>
2022-08-04 04:36:01 -0700

    Delete temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-#GET https://api.spacexdata.com/v5/launches/latest
-import requests
-
-response = requests.get('https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a')
-response.raise_for_status()
-print(response.json()['id'])
-[print(link) for  link in response.json()['links']['flickr']['original']]
-
-
