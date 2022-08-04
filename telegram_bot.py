e408203384831e651489e03911918e532a9a17c1
VneTraffiqua <safe72again@gmail.com>
2022-08-04 04:35:48 -0700

    Update Post_one_img.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,24 +1,30 @@
 import time
 import telegram
-import os
-from dotenv import load_dotenv
 import random
-import get_func
 from pathlib import Path
+import HelperScripts
 
+
+def main():
+    img_path = HelperScripts.get_global_variable('IMG_PATH')
+    bot = telegram.Bot(
+        token=HelperScripts.get_global_variable('TELEGRAM_TOKEN')
+    )
+    img_names = [
+        img_name for img_name in HelperScripts.get_img_names(img_path)
+    ]
+    while True:
+        for img_name in img_names:
+            with open(
+                    Path.cwd() / f'{img_path}' / f'{img_name}', 'rb'
+            ) as document:
+                bot.send_document(
+                    chat_id=HelperScripts.get_global_variable('TG_CHAT_ID'),
+                    document=document
+                )
+            time.sleep(int(HelperScripts.get_global_variable('TIMER')))
+        random.shuffle(img_names)
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    tg_token = os.getenv('TELEGRAM_TOKEN')
-    img_path = os.getenv('IMG_PATH')
-    bot = telegram.Bot(token=tg_token)
-    img_names = [i for i in get_func.get_img_names(img_path)]
-    while True:
-        for img_name in get_func.get_img_names(img_path):
-            bot.send_document(
-                chat_id=os.getenv('TG_CHAT_ID'),
-                document=open(Path.cwd() / f'{img_path}' / f'{img_name}', 'rb')
-            )
-            time.sleep(int(os.getenv('TIMER')))
-        random.shuffle(img_names)
+    main()
