44461c9c26329cb5266c010ab1a44b671b2d3cb7
VneTraffiqua <safe72again@gmail.com>
2022-08-04 04:35:02 -0700

    Update fetch_NASA_APOD_images.py

--- fetch_NASA_APOD_images.py
+++ fetch_NASA_APOD_images.py
@@ -1,30 +1,29 @@
 import requests
-from dotenv import load_dotenv
 from pathlib import Path
-import get_func
-import os
+import HelperScripts
 
 
-def get_APOD():
+def get_apod():
     url_nasa = 'https://api.nasa.gov/planetary/apod'
     settings = {
-        'api_key': nasa_token,
-        'start_date': start_date_apod
+        'api_key': HelperScripts.get_global_variable('NASA_TOKEN'),
+        'start_date': HelperScripts.get_global_variable('START_DATE_APOD')
     }
     response = requests.get(url_nasa, params=settings)
     response.raise_for_status()
-    return [data['url'] for data in response.json() 
-            if data['media_type'] == 'image'
+    return [row['url'] for row in response.json()
+            if row['media_type'] == 'image'
             ]
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    nasa_token = os.getenv('NASA_TOKEN')
-    img_path = os.getenv('IMG_PATH')
-    start_date_apod = os.getenv('START_DATE_APOD')
+def main():
+    img_path = HelperScripts.get_global_variable('IMG_PATH')
     Path(f'{img_path}').mkdir(parents=True, exist_ok=True)
-    for img_num, APOD_url in enumerate(get_APOD(), 1):
+    for img_num, apod_url in enumerate(get_apod(), 1):
         path = Path.cwd() / f'{img_path}' / \
-               f'NASA{img_num}{get_func.get_extension(APOD_url)}'
-        get_func.get_images(path, APOD_url)
+               f'NASA{img_num}{HelperScripts.get_extension(apod_url)}'
+        HelperScripts.get_images(path, apod_url)
+
+
+if __name__ == '__main__':
+    main()
