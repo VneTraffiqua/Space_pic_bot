bde2c77da250c2d654e412c76ae02ff4d2db9694
VneTraffiqua <safe72again@gmail.com>
2022-08-08 11:27:59 -0700

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,6 +2,8 @@ import requests
 from pathlib import Path
 import argparse
 import HelperScripts
+import os
+from dotenv import load_dotenv
 
 
 def get_args():
@@ -19,14 +21,15 @@ def get_url_pic_spacex(spacex_url):
 
 
 def main():
-    img_path = HelperScripts.get_global_variable('IMG_PATH')
+    load_dotenv()
+    img_path = os.getenv('IMG_PATH')
     Path(f'{img_path}').mkdir(parents=True, exist_ok=True)
     launch_id = get_args().launch_id
-    url_launch = f'https://api.spacexdata.com/v5/launches/{launch_id}'
-    for img_num, img_url in enumerate(get_url_pic_spacex(url_launch), 1):
+    launch_url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
+    for img_num, img_url in enumerate(get_url_pic_spacex(launch_url), 1):
         path = Path.cwd() / f'{img_path}' / \
                f'spacex{img_num}{HelperScripts.get_extension(img_url)}'
-        HelperScripts.get_images(path, img_url)
+        HelperScripts.save_images(path, img_url)
 
 
 if __name__ == '__main__':
