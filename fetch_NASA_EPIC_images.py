d9105c7913c13a583ef56e2ee432e97fc10378af
VneTraffiqua <safe72again@gmail.com>
2022-08-08 11:27:50 -0700

    Update fetch_NASA_EPIC_images.py

--- fetch_NASA_EPIC_images.py
+++ fetch_NASA_EPIC_images.py
@@ -1,13 +1,15 @@
 import requests
 from pathlib import Path
 import HelperScripts
+import os
+from dotenv import load_dotenv
 
 
-def get_epic_url():
+def get_epic_url(api_key, start_date):
     url1 = 'https://api.nasa.gov/EPIC/api/natural/images'
     settings = {
-        'api_key': HelperScripts.get_global_variable('NASA_TOKEN'),
-        'start_date': HelperScripts.get_global_variable('START_DATE_EPIC')
+        'api_key': api_key,
+        'start_date': start_date
     }
     response = requests.get(url1, params=settings)
     response.raise_for_status()
@@ -15,19 +17,24 @@ def get_epic_url():
 
 
 def main():
-    img_path = HelperScripts.get_global_variable('IMG_PATH')
+    load_dotenv()
+    img_path = os.getenv('IMG_PATH')
+    epic_start_date = os.getenv('START_DATE_EPIC')
+    nasa_token = os.getenv('NASA_TOKEN')
     Path(f'{img_path}').mkdir(parents=True, exist_ok=True)
-    for img_num, EPIC_data in enumerate(get_epic_url(), 1):
+    for img_num, EPIC_data in enumerate(
+            get_epic_url(nasa_token, epic_start_date), 1
+    ):
         image_id = EPIC_data['image']
-        image_date = EPIC_data['date'].split()[0].split('-')
+        year, month, day = EPIC_data['date'].split()[0].split('-')
         epic_url = f'https://api.nasa.gov/EPIC/archive/natural/' \
-                   f'{image_date[0]}/{image_date[1]}/{image_date[2]}/' \
+                   f'{year}/{month}/{day}/' \
                    f'png/{image_id}.png'
         path = Path.cwd() / f'{img_path}' / f'NASA_EPIC_{img_num}.png'
         settings_epic_url = {
-            'api_key': HelperScripts.get_global_variable('NASA_TOKEN'),
+            'api_key': nasa_token,
         }
-        HelperScripts.get_images(path, epic_url, settings_epic_url)
+        HelperScripts.save_images(path, epic_url, settings_epic_url)
 
 
 if __name__ == '__main__':
